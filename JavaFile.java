67c105be6b66b1234a4c1ac71189a8443dbd6c74
Group 4 <johnmcudo2483@gmail.com>
2021-11-13 14:52:46 +0300

    Editeed new src code

--- JavaFile.java
+++ JavaFile.java
@@ -1,1 +1,20 @@
-hubsdvucauiscniadsc
+package codes;
+
+import java.util.Random;
+
+public class Random_Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random r1,r2;
+		
+		r1=new Random();
+		System.out.println("Random Value 1: "+r1.nextDouble());
+		
+		r2=new Random();
+		System.out.println("Random Value 2: "+r2.nextDouble());
+		
+	}
+
+}
